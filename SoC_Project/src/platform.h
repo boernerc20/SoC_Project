84984289f88ad016050111748265e39b1f408f50
Christopher Boerner <89418636+boernerc20@users.noreply.github.com>
2025-03-10 13:13:18 -0400

    Add files via upload
    functionality for Win, Wx, data_in and ESN first equation

---
+++ SoC_Project/src/platform.h
@@ -0,0 +1,10 @@
+#ifndef __PLATFORM_H_
+#define __PLATFORM_H_
+
+/* Initializes the platform (e.g., caches, etc.) */
+void init_platform(void);
+
+/* Cleans up the platform before exiting */
+void cleanup_platform(void);
+
+#endif /* __PLATFORM_H_ */
